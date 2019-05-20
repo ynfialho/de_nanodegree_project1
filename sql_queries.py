14ae261cc5a2d9fc7d8548faa5d0f08a6d648d47
Yuri Fialho <yuri.fialho@yandeh.com.br>
2019-05-20 08:14:55 -0300

    init commit

---
+++ sql_queries.py
@@ -0,0 +1,55 @@
+# DROP TABLES
+
+songplay_table_drop = "fact_songplay"
+user_table_drop = "dim_user"
+song_table_drop = "dim_song"
+artist_table_drop = "dim_artist"
+time_table_drop = "dim_time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE fact_songplay(
+songplay_id int 
+)
+""")
+
+user_table_create = ("""
+""")
+
+song_table_create = ("""
+""")
+
+artist_table_create = ("""
+""")
+
+time_table_create = ("""
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+""")
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+
+time_table_insert = ("""
+""")
+
+# FIND SONGS
+
+song_select = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
